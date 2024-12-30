529d769e4290d826c11397a12790f2eba4b7d0ae
nasiruddinthander <108251321+N-Thander@users.noreply.github.com>
2024-12-31 03:23:19 +0530

    Update clsTorpedo.py

--- components/clsTorpedo.py
+++ components/clsTorpedo.py
@@ -1,30 +1,27 @@
+
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath('TorpedoDetectionApp_Hitech')))
 
 from imports import *
 
-def clsTorpedo(clsModelPath, imagePath):
-    clsModel = YOLO(clsModelPath)
+def detectTorpedo(detectModelPath, imagePath):
+    detectModel = YOLO(detectModelPath)
     
-    results = clsModel.predict(source=imagePath)
+    results = detectModel(imagePath)
     
     if not results:
-        print("No result from Torpedo Model")
-        return False
+        print("No bounding box")
+        return -99, -99, -99, -99
     
     for result in results:
-        if hasattr(result, 'probs'):
-            top1_class_id = result.probs.top1
-            top1_confidence = result.probs.top1conf
-            
-            # s = f"{top1_class_id} {result.names[top1_class_id]} {top1_confidence:.2f}"
-            
-            if result.names[top1_class_id] == "TorpedoFrame" and top1_confidence >= 0.99:
-                return True
-            
-        else:
-            print("Result dfoes not contain probability information")
+        boxes = result.boxes
+        if not boxes:
+            print("No boxes found")
+            return -99, -99, -99, -99
+        
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0].cpu().detach().numpy().astype(int)
+            return x1, y1, x2, y2
     
-    return False
-
+    return -99, -99, -99, -99
