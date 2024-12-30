417280f06fbd89206a1af448a04a783bcdb58b30
nasiruddinthander <108251321+N-Thander@users.noreply.github.com>
2024-12-31 03:23:00 +0530

    Update clsTorpedo.py

--- components/clsTorpedo.py
+++ components/clsTorpedo.py
@@ -3,30 +3,11 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath('TorpedoDetectionApp_Hitech')))
 
 from imports import *
-from typing import Union
 
-def clsTorpedo(clsModelPath: str, imagePath: str) -> Union[bool, None]:
-    """
-    Classify an image using a YOLO model to detect 'TorpedoFrame'.
-
-    Parameters:
-    clsModelPath (str): Path to the YOLO model.
-    imagePath (str): Path to the image to be classified.
-
-    Returns:
-    bool: True if 'TorpedoFrame' is detected with 0.99 confidence, False otherwise.
-    """
-    try:
-        clsModel = YOLO(clsModelPath)
-    except Exception as e:
-        print(f"Error loading model: {e}")
-        return None
+def clsTorpedo(clsModelPath, imagePath):
+    clsModel = YOLO(clsModelPath)
     
-    try:
-        results = clsModel.predict(source=imagePath)
-    except Exception as e:
-        print(f"Error during prediction: {e}")
-        return None
+    results = clsModel.predict(source=imagePath)
     
     if not results:
         print("No result from Torpedo Model")
@@ -37,9 +18,13 @@ def clsTorpedo(clsModelPath: str, imagePath: str) -> Union[bool, None]:
             top1_class_id = result.probs.top1
             top1_confidence = result.probs.top1conf
             
-            if result.names[top1_class_id] == "TorpedoFrame" and top1_confidence == 0.99:
+            # s = f"{top1_class_id} {result.names[top1_class_id]} {top1_confidence:.2f}"
+            
+            if result.names[top1_class_id] == "TorpedoFrame" and top1_confidence >= 0.99:
                 return True
+            
         else:
-            print("Result does not contain probability information")
+            print("Result dfoes not contain probability information")
     
     return False
+
